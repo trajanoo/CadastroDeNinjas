a7d2124cd1066d19d1df174ff86ef81a91b15ceb
trajanoo <brunotrajano.dev@gmail.com>
2025-02-14 03:10:08 -0300

    chore: add environment variables; add .env to .ignore

--- src/main/java/com/spring/CadastroNinja/Missoes/MissoesModel.java
+++ src/main/java/com/spring/CadastroNinja/Missoes/MissoesModel.java
@@ -1,10 +1,7 @@
 package com.spring.CadastroNinja.Missoes;
 
 import com.spring.CadastroNinja.Ninjas.NinjaModel;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,8 +17,10 @@ import java.util.List;
 public class MissoesModel {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nomeMissao;
+    @Enumerated(EnumType.STRING)
     private Rank rank;
 
     @OneToMany(mappedBy = "missoes")
---
+++ src/main/java/com/spring/CadastroNinja/Missoes/MissoesRepository.java
@@ -0,0 +1,6 @@
+package com.spring.CadastroNinja.Missoes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissoesRepository extends JpaRepository<MissoesModel, Long> {
+}
