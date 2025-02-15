68b9873ccbe455b20a47a8dcf188f5711387a576
trajanoo <brunotrajano.dev@gmail.com>
2025-02-15 16:51:38 -0300

    feat: add missoes Controller

---
+++ src/main/java/com/spring/CadastroNinja/Missoes/MissoesController.java
@@ -0,0 +1,31 @@
+package com.spring.CadastroNinja.Missoes;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes")
+public class MissoesController {
+    // Create
+    @PostMapping("/criarMissoes")
+    public String criar() {
+        return "Missão criado com sucesso";
+    }
+
+    // Read
+    @GetMapping("/listarMissoes")
+    public String listar() {
+        return "Missões listadas com sucesso";
+    }
+
+    // Update
+    @PutMapping("/atualizarMissao")
+    public String atualizar() {
+        return "Missões atualizadas com sucesso";
+    }
+
+    // Delete
+    @DeleteMapping("/removerMissao")
+    public String remover() {
+        return "Missões removidas com sucesso";
+    }
+}
--- src/main/java/com/spring/CadastroNinja/Ninjas/NinjaController.java
+++ src/main/java/com/spring/CadastroNinja/Ninjas/NinjaController.java
@@ -1,15 +1,34 @@
 package com.spring.CadastroNinja.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
 public class NinjaController {
 
-    @GetMapping("/ninja")
-    public String Ninja() {
-        return "Essa é a página Ninja";
+    // CRUD: Create, Read, Update, Delete
+
+    // Create
+    @PostMapping("/criar")
+    public String criar() {
+        return "Ninja criado com sucesso";
+    }
+
+    // Read
+    @GetMapping("/listarID")
+    public String mostrarNinjasPorId() {
+        return "Ninjas mostrados por ID";
+    }
+
+    // Update
+    @PutMapping("/atualizarID")
+    public String atualizar() {
+        return "Alterar ninja por ID";
+    }
+
+    // Delete
+    @DeleteMapping("/removerID")
+    public String remover() {
+        return "Remover ninja por ID";
     }
 }
